63d21618525cd0ec0cc321b4d33da51d361c0804
Prabhu Nath Tiwary <prabhunt@micelio.local>
2021-03-29 14:29:25 +0530

    created home page

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:uber_eats_prabhu/pages/home_page.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+));
